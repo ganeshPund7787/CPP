73a94279a06de8fb1fb291d768162383ffc106b3
ganeshPund7787 <ganupund7787@gmail.com>
2025-01-21 09:23:19 +0530

    Access Specifire

--- Practice/oops/Access-Specifire/private.cpp
+++ Practice/oops/Access-Specifire/private.cpp
@@ -1,33 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Computer {
-    private:
+private:
     double price;
     string company;
     string storage;
 
-    public:
-    Computer(double price, string company, string storage){
+public:
+    Computer(double price, string company, string storage) {
         this->price = price;
         this->company = company;
         this->storage = storage;
     }
 
-    void display(){
-        cout << this->price = price << endl;
-        cout << this->company = company << endl; 
-        cout << this->storage = storage << endl; 
+    void display() {
+        cout << "Price: " << price << endl;
+        cout << "Company: " << company << endl;
+        cout << "Storage: " << storage << endl;
     }
 };
 
-
-int main(){
-
-   Computer ob1(20000, "Lenovo", "512GB");
-    
+int main() {
+    Computer ob1(20000, "Lenovo", "512GB");
+    ob1.display();
 
     return 0;
 }
-
-
---
+++ Practice/oops/Access-Specifire/protected.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Computer {
+    protected:
+    double price;
+    string company;
+    string storage;
+    Computer(){}
+    Computer(double price, string company, string storage){
+        this->price = price;
+        this->company = company;
+        this->storage = storage;
+    }
+};
+
+class lapTop: protected Computer {
+    protected:
+    string battery;
+    bool charger;
+};
+
+int main(){
+
+    Computer ob1(20000, "Lenovo", "512GB");
+    cout << endl << "This is an first Object: " << endl;
+    cout << ob1.price << endl;
+    cout << ob1.company << endl;
+    cout << ob1.storage << endl;
+
+    lapTop ob2;
+    cout << endl << "This is an second Object: " << endl;
+     ob2.charger = true;
+     ob2.battery = "12VH";
+     ob2.price  = 400000;
+     ob2.company = "Dell";
+     ob2.storage = "256GB";
+ 
+    cout << ob2.charger << endl;
+    cout << ob2.battery << endl;
+    cout << ob2.price << endl;
+    cout << ob2.company << endl;
+    cout << ob2.storage << endl;
+    
+    return 0;
+}
+
+
